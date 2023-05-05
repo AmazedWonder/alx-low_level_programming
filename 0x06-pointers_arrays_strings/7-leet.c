931fc12cf35be841056eb52e489a335d2424efe3
Wonder <giftedwonda@gmail.com>
2023-05-04 21:44:51 -0700

    Write a function that encodes a string into 1337,Aa to 4,Ee to 3,Oo to 0,Tt to 7, Ll to 1

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * leet - Encodes a string to 1337
+ * @str: string of ASCII code
+ * Return: ASCII string
+ */
+char *leet(char *str)
+{
+	int c1 = 0, c2;
+	char leet[8] = {'O','L','?','E','A','?','?','T'};
+
+	while (str[c1])
+	{
+		for (c2 = 0; c2 <= 7; c2++)
+		{
+			if (str[c1] == leet[c2] ||
+			str[c1] - 32 == leet[c2])
+				str[c1] = c2 + 48;
+		}
+		c1++;
+	}
+	return (str);
+}

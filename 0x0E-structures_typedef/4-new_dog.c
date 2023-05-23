2c913b1dbfc7f71506b4bab7322e7bf9a61ca065
Wonder <giftedwonda@gmail.com>
2023-05-22 18:33:57 -0700

    store a copy of name and owner in function

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "dog.h"
+
+/**
+ * new_dog - new dog with the details
+ * @name: Pointer to a character name of the dog
+ * @age: Floating-point value,age of the dog
+ * @owner: Pointer to a char name of the owner of the dog
+ * Return: Pointer to the newly created dog, or NULL if memory allocation fails
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+dog_t *new_dog = malloc(sizeof(dog_t));
+
+if (name == NULL || age < 0 || owner == NULL)
+return (NULL);
+
+new_dog->name = strdup(name);
+if (new_dog->name == NULL)
+{
+free(new_dog);
+return (NULL);
+}
+
+new_dog->owner = strdup(owner);
+if (new_dog->owner == NULL)
+{
+free(new_dog->name);
+free(new_dog);
+return (NULL);
+}
+
+new_dog->age = age;
+
+return (new_dog);
+}
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -26,5 +26,5 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 
 void print_dog(struct dog *d);
 
-dog_t *doggy(struct dog *d, char *name, float age, char *owner);
+dog_t *new_dog(char *name, float age, char *owner);
 #endif /* DOG_H ndef */

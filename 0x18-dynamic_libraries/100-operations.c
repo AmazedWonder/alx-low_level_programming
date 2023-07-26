ea647852befbaee6e02a7dac87a858243d602642
AmazedWonder <giftedwonda@gmail.com>
2023-07-26 05:02:50 +0100

    Let's call C functions from Python.Create .so file from .c

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+
+/**
+ * add - function adds two integers
+ *
+ * @a: the first integer
+ * @b: the second integer
+ *
+ * Return: sum of the two integers
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - function subtracts two integers
+ *
+ * @a: the first integer
+ * @b: the second integer
+ *
+ * Return: difference between two integers
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - function multiplies two integers
+ *
+ * @a: the first integer
+ * @b: the second integer
+ *
+ * Return: product of the two integers
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - function divides two integers
+ *
+ * @a: the first integer
+ * @b: the second integer
+ *
+ * Return: quotient of the two integers
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - function computes the modulo of two integers
+ *
+ * @a: the first integer
+ * @b: the second integer
+ *
+ * Return: modulo/remainder of two integers
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}

